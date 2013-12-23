2225c7a5465adaa2ef565c625605a041b3f263fe
Victor Gemini <victor.gemini@gmail.com>
2013-12-24 07:18:38 +1100

    2013-12-24

--- xor.h
+++ xor.h
@@ -5,7 +5,7 @@
 #define BUFSIZE 16384
 typedef unsigned char byte;
 
-int xor_encode(FILE *in, FILE *out, char *key);
+int xor_encode(FILE *in, FILE *out, byte *key, size_t keysize);
 int xor_encode_pad(FILE *in, FILE *out, FILE *pad);
 
 #endif /* __XOR_H */
